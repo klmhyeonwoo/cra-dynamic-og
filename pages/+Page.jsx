7e05ce1d46a41d1d084796ed14daa9fcd8530279
klmhyeonwoo <gentlemonster77@likelion.org>
2024-06-30 00:11:26 +0900

    vike

--- pages/+Page.jsx
+++ pages/+Page.jsx
@@ -8,12 +8,7 @@ import { Link, Route, Routes } from "react-router-dom";
 function Page() {
   return (
     <>
-      <b>
-        <i>
-          Time elapsed: <TimeElapsed />
-        </i>
-        <Counter />
-      </b>
+      <b></b>
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -21,11 +16,14 @@ function Page() {
         <li>
           <Link to="/about">About</Link>
         </li>
+        <li>
+          <Link to="/share">Share</Link>
+        </li>
       </ul>
       <hr />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/share" element={<About />} />
+        <Route path="/about" element={<About />} />
         <Route path="/share" element={<Share />} />
       </Routes>
     </>
@@ -37,23 +35,11 @@ function Home() {
     <div>
       <h2>Home</h2>
       <p>Example of client-side routing with React Router and SSR.</p>
-      <p>
-        This page is rendered to HTML, see <Code>view-source:http://localhost:3000</Code>.
-      </p>
     </div>
   );
 }
 
 function About() {
-  const document = useDocument();
-  document.title = "Awesome Product";
-  document.head.add(<meta name="description" content="This product helps you with..." />);
-  document.head.add(
-    <meta
-      property="og:image"
-      content="http://thumbnail.10x10.co.kr/webimage/image/basic600/408/B004089332-1.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false"
-    />,
-  );
   return (
     <>
       <h2>About</h2>
