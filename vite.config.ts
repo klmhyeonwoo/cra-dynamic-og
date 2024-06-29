f150bc172fb404bd11dcb32b1aa7c4fc72784460
klmhyeonwoo <gentlemonster77@likelion.org>
2024-06-29 21:08:06 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { htmlPrerender } from "vite-plugin-html-prerender";
 import path from "path";
+import vitePrerender from "vite-plugin-prerender";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr()],
+  plugins: [
+      svgr(),
+      htmlPrerender({
+          /**
+           * Required: Output directory of `vite build`.
+           */
+          staticDir: path.join(__dirname, "dist"),
+
+          /**
+           * Required: List of routes to pre-render.
+           */
+          routes: ["/", "/share"],
+
+          /**
+           * Optional: Query selector to wait for. Defaults to `#root`.
+           */
+          selector: "main",
+
+          /**
+           * Optional: To minify html. Uses https://github.com/kangax/html-minifier.
+           */
+          minify: {
+              collapseBooleanAttributes: true,
+              collapseWhitespace: true,
+              decodeEntities: true,
+              keepClosingSlash: true,
+              sortAttributes: true
+          }
+      })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
